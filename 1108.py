94251fcb0beb5c4c274c1bcefe6ca8bff860772b
Ryu <eostmghf01@gmail.com>
2019-11-09 14:10:24 +0900

    Add files via upload

---
+++ 1108.py
@@ -0,0 +1,8 @@
+#오름차순인 숫자 nums 배열과 찾아야할 target을 인자로 주면,
+#target이 몇 번째 index인지 return 해주세요.  
+
+#Input: nums = [-1,0,3,5,9,12], target = 9
+#Output: 4
+#Input: nums = [-1,0,3,5,9,12], target = 2
+#Output: -1
+# 설명: 찾지 못하면 -1로 return 해주세요.
