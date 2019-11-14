54d6de8b3941fd9e3bf628175cb7b08e92d22edc
Ryu <eostmghf01@gmail.com>
2019-11-14 19:05:25 +0900

    Add files via upload

---
+++ 1114.py
@@ -0,0 +1,6 @@
+# linked list를 만들 수 있는 MyLinkedList 클래스를 설계해봅시다.
+# singly linked list 로 해도 되고, doubly linked list 로 구현하셔도 됩니다.
+
+# singly linked list를 구현하려면 val과 next라는 속성이 있어야 합니다.
+# val: 현재 node의 value
+# next: 다음 node를 가르키는 pointer(reference)
