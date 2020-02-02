cf088a71c7591fd1d8e9b178ff2035139ed32130
Ryu <eostmghf01@gmail.com>
2020-02-02 19:33:58 +0900

    SpanningTree

---
+++ graph(SpanningTree).py
@@ -0,0 +1,15 @@
+# 최소 신장 트리 (Minimum Spanning Tree)
+# 원래의 그래프의 모든 노드가 연결되어 있으면서  트리의 속성을 만족
+
+# 신장 트리가 되기 위한 조건 3가지
+# 1) 본래의 그래프의 모든 노드를 포함해야 함
+# 2) 모든 노드가 서로 연결
+# 3) 트리의 속성을 만족시킴(사이클이 존재하지 않음)
+
+# 최소 신장트리(Minimum Spanning Tree)
+# spanning tree 중 Edge(간선)의 가중치의 합이 최소인 spanning tree를 지칭
+
+# 최소 신장 트리 알고리즘
+# 1) Kruskal’s algorithm (크루스칼 알고리즘)
+# 2) Prim's algorithm (프림 알고리즘)
+
